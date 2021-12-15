6ef7537a74bf96094dcc0452dd3fdf7629fd89ea
tejothanmaya <sreevallichowdary99@gmail.com>
2021-12-15 20:07:07 +0530

    Added SDK

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { Text, View, StyleSheet } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Constants from 'expo-constants';
 
-
 // You can import from local files
 import HomeScreen from './screens/HomeScreen';
 
